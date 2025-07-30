a0ca0eebb6c6e06fd709153967c09714226e7a0e
affan818 <ansariaffan818@gmail.com>
2025-07-30 11:43:42 +0530

    complete structure

--- src/component/TrustedClients.jsx
+++ src/component/TrustedClients.jsx
@@ -4,24 +4,30 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // 👇  Add your logo imports / URLs here
-import arisiaLogo from "../assets/favicon.jpg";
-import abdulLogo from "../assets/favicon.jpg";
-import alphaLogo from "../assets/favicon.jpg";
-import stellarLogo from "../assets/favicon.jpg";
-import dnaLogo from "../assets/favicon.jpg";
-import logo2 from "../assets/favicon.jpg";
-import logo3 from "../assets/favicon.jpg";
-import logo4 from "../assets/favicon.jpg";
+import devprath from "../assets/clients/devprath.jpg";
+import gandhi from "../assets/clients/gandhi.jpg";
+import govardhan from "../assets/clients/govardhan.jpg";
+import kukrejaOne from "../assets/clients/kukreja-2.jpg";
+import kukreja from "../assets/clients/kukreja.jpg";
+import mittal from "../assets/clients/mittal.jpg";
+import nanik from "../assets/clients/nanik.jpg";
+import navnirman from "../assets/clients/navnirman.jpg";
+import pankaj from "../assets/clients/pankaj.jpg";
+import prasanna from "../assets/clients/prasanna.jpg";
+import shivganga from "../assets/clients/shivganga.jpg";
 
 const logos = [
-  arisiaLogo,
-  abdulLogo,
-  alphaLogo,
-  stellarLogo,
-  dnaLogo,
-  logo2,
-  logo3,
-  logo4,
+  kukrejaOne,
+  kukreja,
+  mittal,
+  navnirman,
+  pankaj,
+  nanik,
+  prasanna,
+  shivganga,
+  govardhan,
+  gandhi,
+  devprath,
 ];
 
 export default function TrustedClients() {
@@ -31,33 +37,36 @@ export default function TrustedClients() {
         Our trusted Clients
       </h2>
 
-      {/* Carousel */}
-      <Swiper
-        modules={[Autoplay, Pagination]}
-        slidesPerView={2}
-        spaceBetween={20}
-        loop
-        autoplay={{ delay: 2500, disableOnInteraction: false }}
-        pagination={{ clickable: true }}
-        breakpoints={{
-          640: { slidesPerView: 3 },
-          768: { slidesPerView: 4 },
-          1024: { slidesPerView: 5 },
-        }}
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"
-      >
-        {logos.map((src, i) => (
-          <SwiperSlide key={i}>
-            <div className="flex justify-center items-center h-20 sm:h-24 ">
-              <img
-                src={src}
-                alt="Client Logo"
-                className="max-h-full max-w-full object-contain grayscale hover:grayscale-0 transition pb-10"
-              />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="relative pb-16">
+        {" "}
+        {/* Give enough space for pagination */}
+        <Swiper
+          modules={[Autoplay, Pagination]}
+          slidesPerView={2}
+          spaceBetween={20}
+          loop
+          autoplay={{ delay: 2500, disableOnInteraction: false }}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            640: { slidesPerView: 3 },
+            768: { slidesPerView: 4 },
+            1024: { slidesPerView: 5 },
+          }}
+          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 custom-swiper"
+        >
+          {logos.map((src, i) => (
+            <SwiperSlide key={i}>
+              <div className="flex justify-center items-center h-32 sm:h-40 md:h-48 lg:h-56">
+                <img
+                  src={src}
+                  alt="Client Logo"
+                  className="max-h-full max-w-full object-contain grayscale hover:grayscale-0 transition duration-300"
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
   );
 }
